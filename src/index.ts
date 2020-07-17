a8e3a969ebc8d596943eccb6476980eda2026a92
MrDrummer25 <mrdrummer.programming@hotmail.com>
2020-07-18 00:37:21 +0100

    kinda functional code

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+export interface Breakpoint {
+  lessThan: number
+  every: number
+}
+
+export const sum = (arr: number[]): number => arr.reduce((a, b) => a + b, 0)
+
+export type CountdownCallback = () => void
+
+export const getUnix = (): number => Date.now() / 1000
+
+export const generateIntervals = (breakpoints: Breakpoint[], targetDate: Date, callback: CountdownCallback): number[] => {
+  const secondsToTarget = Math.floor((targetDate.getTime() / 1000) - getUnix())
+  // return test(breakpoints, secondsToTarget)
+  // console.log("secondsToTarget :", secondsToTarget)
+
+  const intervals: number[] = []
+  let leftToFit = secondsToTarget
+  for (const i in breakpoints) {
+    const bp = breakpoints[i]
+    const nextBp = breakpoints[parseInt(i) + 1]
+
+
+
+    console.log("bp :", bp)
+    const fits = Math.floor(leftToFit / bp.every)
+    if (fits === 1 && bp.every === nextBp?.lessThan) continue
+    console.log(`fits : ${ fits }`)
+
+    intervals.push(...Array(fits).fill(bp.every))
+
+    leftToFit = leftToFit - (bp.every * fits)
+  }
+  return intervals
+}
